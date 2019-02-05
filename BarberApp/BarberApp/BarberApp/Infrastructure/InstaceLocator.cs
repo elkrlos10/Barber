e693916c04eaf7b020b7f22799b9c9d3d86bc106
elkrlos10 <www.kliche10@gmail.com>
2019-02-04 23:36:49 -0500

    Inicio Proyecto

---
+++ BarberApp/BarberApp/BarberApp/Infrastructure/InstaceLocator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BarberApp.Infrastructure
+{
+    using ViewModel;
+   
+    public class InstaceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstaceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
